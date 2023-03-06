4431cd0b8e61e9b9d15f386f07e9724d9704648c
Вячеслав Петров <213xixixi@gmail.com>
2023-03-06 14:13:03 +0300

    review1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,12 +1,14 @@
-from api.views import CommentViewSet, GroupViewSet, PostViewSet
-from django.urls import include, path
 from rest_framework import routers
 from rest_framework.authtoken import views
 
-router = routers.DefaultRouter()
-router.register(r'posts', PostViewSet, basename='posts')
-router.register(r'groups', GroupViewSet, basename='groups')
-router.register(
+from django.urls import include, path
+
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
+
+v1_router = routers.DefaultRouter()
+v1_router.register(r'posts', PostViewSet, basename='posts')
+v1_router.register(r'groups', GroupViewSet, basename='groups')
+v1_router.register(
     r'^posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comment'
@@ -14,5 +16,5 @@ router.register(
 
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
-    path('v1/', include(router.urls)),
+    path('v1/', include(v1_router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,10 @@
+from rest_framework import viewsets
 from django.core.exceptions import PermissionDenied
 from django.shortcuts import get_object_or_404
-from posts.models import Group, Post
-from rest_framework import viewsets
 
-from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+from api.serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+from posts.models import Group, Post
 
 
 class PostViewSet(viewsets.ModelViewSet):
@@ -16,7 +17,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(PostViewSet, self).perform_update(serializer)
+        super().perform_update(serializer)
 
     def perform_destroy(self, instance):
         if instance.author != self.request.user:
@@ -43,7 +44,7 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_update(self, serializer):
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
-        super(CommentViewSet, self).perform_update(serializer)
+        super().perform_update(serializer)
 
     def perform_destroy(self, instance):
         if instance.author != self.request.user:
