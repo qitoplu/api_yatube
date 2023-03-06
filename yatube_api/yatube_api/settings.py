4b3fae3169f5846befa8ff42c8c74d5390ab1af9
Вячеслав Петров <213xixixi@gmail.com>
2023-03-06 11:10:58 +0300

    pep8 pt2

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -114,15 +114,16 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES':
+    [
         'rest_framework.permissions.IsAuthenticated',
     ],
 
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    'DEFAULT_AUTHENTICATION_CLASSES':
+    [
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
-
 
 SIMPLE_JWT = {
    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
