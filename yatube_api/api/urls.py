a2849ce8716a7b84147b7d085b3d9245adacba28
Вячеслав Петров <213xixixi@gmail.com>
2023-03-06 12:06:14 +0300

    d1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,6 +15,4 @@ router.register(
 urlpatterns = [
     path('v1/api-token-auth/', views.obtain_auth_token),
     path('v1/', include(router.urls)),
-    path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.jwt')),
 ]
