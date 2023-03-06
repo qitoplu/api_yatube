1be1fb2030934a371fbff9b27f725ce66a53f432
Вячеслав Петров <213xixixi@gmail.com>
2023-03-06 11:18:19 +0300

    pep8 pt4

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -25,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'djoser',
     'posts.apps.PostsConfig',
     'api.apps.ApiConfig',
 ]
